ac0234c94cc86de5f963a688e58e161707d5e3e9
kemar <marc@marcarea.com>
2014-12-26 20:14:15 +0100

    Add unit tests for the default markup structure

---
+++ test/unit/countdown_core.js
@@ -0,0 +1,26 @@
+QUnit.test("Markup structure", function (assert) {
+
+    var c = $('#countdown-test').countDown();
+    var timeElement = $('#countdown-test > .countdown');
+
+    assert.ok(timeElement.is('time'), "A time element has been created");
+
+    assert.equal(typeof(timeElement.attr('datetime')), 'string', "A datetime attribute has been created");
+
+    assert.equal(timeElement.find('.item').length, 4, ".item elements exist, correct number");
+    assert.equal(timeElement.find('.separator').length, 3, ".separator elements exist, correct number");
+
+    // Days infos should be hidden.
+    assert.equal(timeElement.find('.item:visible').length, 3, "Visible .item elements, correct number");
+    assert.equal(timeElement.find('.separator:visible').length, 2, "Visible .separator elements, correct number");
+
+    assert.equal(timeElement.find('.separator').eq(0).text(), ',', "Default separator for days");
+    assert.equal(timeElement.find('.separator').eq(1).text(), ':', "Default separator for hours");
+    assert.equal(timeElement.find('.separator').eq(2).text(), ':', "Default separator for minutes");
+
+    assert.equal(timeElement.find('.label-dd').text(), 'days', "Default days label");
+    assert.equal(timeElement.find('.label-hh').text(), 'hours', "Default hours label");
+    assert.equal(timeElement.find('.label-mm').text(), 'minutes', "Default minutes label");
+    assert.equal(timeElement.find('.label-ss').text(), 'seconds', "Default seconds label");
+
+});
