86db381187fdfee28e52bbfd0b63cafb52f954e1
kemar <marc@marcarea.com>
2014-12-26 14:05:27 +0100

    Updated to the last version of the code.

--- jquery.countdown.js
+++ jquery.countdown.js
@@ -1,5 +1,5 @@
 /*
- * jQuery Countdown - v1.2.2
+ * jQuery Countdown - v1.2.3
  * http://github.com/kemar/jquery.countdown
  * Licensed MIT
  */
@@ -130,23 +130,23 @@
                 return;
             }
             if (this.element.attr('datetime')) {  // Try to parse the datetime attribute first.
-                this.end_date = this.parseEndDate(this.element.attr('datetime'));
+                this.endDate = this.parseEndDate(this.element.attr('datetime'));
             }
-            if (this.end_date === undefined) {  // Fallback on the text content.
-                this.end_date = this.parseEndDate(this.element.text());
+            if (this.endDate === undefined) {  // Fallback on the text content.
+                this.endDate = this.parseEndDate(this.element.text());
             }
-            if (this.end_date === undefined) {  // Unable to parse a date.
+            if (this.endDate === undefined) {  // Unable to parse a date.
                 return;
             }
             if (this.element.is('time')) {
-                this.time_element = this.element;
+                this.timeElement = this.element;
             } else {
-                this.time_element = $('<time></time>');
-                this.element.html(this.time_element);
+                this.timeElement = $('<time></time>');
+                this.element.html(this.timeElement);
             }
             this.markup();
-            this.set_timeout_delay = this.sToMs(1);
-            this.time_element.bind('time.elapsed', this.options.onTimeElapsed);
+            this.setTimeoutDelay = this.sToMs(1);
+            this.timeElement.bind('time.elapsed', this.options.onTimeElapsed);
             this.doCountDown();
         },
 
@@ -183,27 +183,32 @@
         //
         // https://html.spec.whatwg.org/multipage/infrastructure.html#valid-duration-string
         // http://en.wikipedia.org/wiki/ISO_8601#Durations
-        // i.e.: P2DT20H00M10S, PT01H01M15S, PT00M10S, P2D
+        // i.e.: P2DT20H00M10S, PT01H01M15S, PT00M10S, P2D, P2DT20H00M10.55S
         //
         // RegExp:
         // /^
-        //     P            => duration designator (historically called "period")
-        //     (?:(\d+)D)?  => (days) followed by the letter "D" (optional)
-        //     T?           => the letter "T" that precedes the time components of the representation (optional)
-        //     (?:(\d+)H)?  => (hours) followed by the letter "H" (optional)
-        //     (?:(\d+)M)?  => (minutes) followed by the letter "M" (optional)
-        //     (?:(\d+)S)?  => (seconds) followed by the letter "S" (optional)
+        //    P                     => duration designator (historically called "period")
+        //    (?:(\d+)D)?           => (days) followed by the letter "D" (optional)
+        //    T?                    => the letter "T" that precedes the time components of the representation (optional)
+        //    (?:(\d+)H)?           => (hours) followed by the letter "H" (optional)
+        //    (?:(\d+)M)?           => (minutes) followed by the letter "M" (optional)
+        //    (
+        //         ?:(\d+)          => (seconds) (optional)
+        //         (?:\.(\d{1,3}))  => (milliseconds) (optional)
+        //         ?S               => followed by the letter "S" (optional)
+        //    )?                    => (optional)
         // $/
         parseDuration: function (str) {
-            var timeArray = str.match(/^P(?:(\d+)D)?T?(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$/);
+            var timeArray = str.match(/^P(?:(\d+)D)?T?(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)(?:\.(\d{1,3}))?S)?$/);
             if (timeArray) {
-                var d, dd, hh, mm, ss;
-                d = new Date();
+                var d, dd, hh, mm, ss, ms;
                 dd = timeArray[1] ? this.dToMs(timeArray[1]) : 0;
                 hh = timeArray[2] ? this.hToMs(timeArray[2]) : 0;
                 mm = timeArray[3] ? this.mToMs(timeArray[3]) : 0;
                 ss = timeArray[4] ? this.sToMs(timeArray[4]) : 0;
-                d.setTime(d.getTime() + dd + hh + mm + ss);
+                ms = timeArray[5] ? parseInt(timeArray[5], 10) : 0;
+                d = new Date();
+                d.setTime(d.getTime() + dd + hh + mm + ss + ms);
                 return d;
             }
         },
@@ -246,30 +251,24 @@
                 // +05:30 => ["+05:30", "+", "05", "30"] => -19800000
                 var offset = timeArray[8] ? timeArray[8].match(/^([\+\-])?(\d{2}):?(\d{2})$/) : undefined;
 
-                // Difference, in milliseconds, between UTC and time based on the values of timeArray.
+                // Time difference between UTC and the given time zone in milliseconds.
                 var utcOffset = 0;
                 if (offset) {
                     utcOffset = this.hToMs(offset[2]) + this.mToMs(offset[3]);
                     utcOffset = (offset[1] === '-') ? utcOffset : -utcOffset;
                 }
 
-                // Set date and time based on the values of timeArray (in the current locale time zone).
-                var d = new Date();
-                d.setHours(timeArray[4] || 0);
-                d.setMinutes(timeArray[5] || 0);
-                d.setSeconds(timeArray[6] || 0);
-                d.setMilliseconds(timeArray[7] || 0);
-                d.setDate(timeArray[3]);
-                d.setMonth(timeArray[2] - 1);
-                d.setFullYear(timeArray[1]);
+                var d, yy, mo, dd, hh, mm, ss, ms;
+                yy = timeArray[1];
+                mo = timeArray[2] - 1;
+                dd = timeArray[3];
+                hh = timeArray[4] || 0;
+                mm = timeArray[5] || 0;
+                ss = timeArray[6] || 0;
+                ms = timeArray[7] || 0;
+                d = new Date(Date.UTC(yy, mo, dd, hh, mm, ss, ms));
 
-                // Difference, in milliseconds, between UTC and the time based on the values of timeArray.
-                var localUtcOffset = this.mToMs(d.getTimezoneOffset());
-
-                if (localUtcOffset !== utcOffset) {
-                    var utcTime = d.getTime();
-                    d.setTime(utcTime + utcOffset - localUtcOffset);
-                }
+                d.setTime(d.getTime() + utcOffset);
 
                 return d;
 
@@ -279,16 +278,18 @@
         // Convert a string representing a human readable duration to a Date object.
         // Limited to days, hours, minutes and seconds.
         //
-        // 600 days, 3:59:12 => ["600 days, 3:59:12", "600", "3", "59", "12"]
-        //           3:59:12 => ["3:59:12", undefined, "3", "59", "12"]
-        //             00:01 => ["00:01", undefined, "00", "01", undefined]
-        //          00:00:59 => ["00:00:59", undefined, "00", "00", "59"]
-        //         240:00:59 => ["240:00:59", undefined, "240", "00", "59"]
-        //         4h 18m 3s => ["4h 18m 3s", undefined, "4", "18", "3"]
-        //     1d 0h 00m 59s => ["1d 0h 00m 59s", "1", "0", "00", "59"]
-        //             2h 0m => ["2h 0m", undefined, "2", "0", undefined]
-        //         24h00m59s => ["24h00m59s", undefined, "24", "00", "59"]
-        //      12:30:39.929 => ["12:30:39.929", undefined, "12", "30", "39"]
+        // Hours and minutes are mandatory.
+        //
+        // 600 days, 3:59:12 => ["600 days, 3:59:12", "600", "3", "59", "12", undefined]
+        //           3:59:12 => ["3:59:12", undefined, "3", "59", "12", undefined]
+        //             00:01 => ["00:01", undefined, "00", "01", undefined, undefined]
+        //          00:00:59 => ["00:00:59", undefined, "00", "00", "59", undefined]
+        //         240:00:59 => ["240:00:59", undefined, "240", "00", "59", undefined]
+        //         4h 18m 3s => ["4h 18m 3s", undefined, "4", "18", "3", undefined]
+        //     1d 0h 00m 59s => ["1d 0h 00m 59s", "1", "0", "00", "59", undefined]
+        //             2h 0m => ["2h 0m", undefined, "2", "0", undefined, undefined]
+        //         24h00m59s => ["24h00m59s", undefined, "24", "00", "59", undefined]
+        //      12:30:39.929 => ["12:30:39.929", undefined, "12", "30", "39", "929"]
         //
         // RegExp:
         // /^
@@ -296,18 +297,19 @@
         //     (\d+)[h:]\s?     => (hours) followed by "h" or ":" and an optional space
         //     (\d+)[m:]?\s?    => (minutes) followed by "m" or ":" and an optional space
         //     (\d+)?[s]?       => (seconds) followed by an optional space (optional)
-        //     (?:\.\d{1,3})?   => full stop character (.) and fractional part of second (optional)
+        //     (?:\.(\d{1,3}))? => (milliseconds) full stop character (.) and fractional part of second (optional)
         // $/
         parseHumanReadableDuration: function (str) {
-            var timeArray = str.match(/^(?:(\d+).+\s)?(\d+)[h:]\s?(\d+)[m:]?\s?(\d+)?[s]?(?:\.\d{1,3})?$/);
+            var timeArray = str.match(/^(?:(\d+).+\s)?(\d+)[h:]\s?(\d+)[m:]?\s?(\d+)?[s]?(?:\.(\d{1,3}))?$/);
             if (timeArray) {
-                var d, dd, hh, mm, ss;
+                var d, dd, hh, mm, ss, ms;
                 d = new Date();
                 dd = timeArray[1] ? this.dToMs(timeArray[1]) : 0;
                 hh = timeArray[2] ? this.hToMs(timeArray[2]) : 0;
                 mm = timeArray[3] ? this.mToMs(timeArray[3]) : 0;
                 ss = timeArray[4] ? this.sToMs(timeArray[4]) : 0;
-                d.setTime(d.getTime() + dd + hh + mm + ss);
+                ms = timeArray[5] ? parseInt(timeArray[5], 10) : 0;
+                d.setTime(d.getTime() + dd + hh + mm + ss + ms);
                 return d;
             }
         },
@@ -332,22 +334,23 @@
             return parseInt(d, 10) * 24 * 60 * 60 * 1000;
         },
 
-        // Returns the seconds (0-59) of the specified timedelta expressed in milliseconds.
+        // Extract seconds (0-59) from the given timedelta expressed in milliseconds.
+        // A timedelta represents a duration, the difference between two dates or times.
         msToS: function (ms) {
             return parseInt((ms / 1000) % 60, 10);
         },
 
-        // Returns the minutes (0-59) of the specified timedelta expressed in milliseconds.
+        // Extract minutes (0-59) from the given timedelta expressed in milliseconds.
         msToM: function (ms) {
             return parseInt((ms / 1000 / 60) % 60, 10);
         },
 
-        // Returns the hours (0-23) of the specified timedelta expressed in milliseconds.
+        // Extract hours (0-23) from the given timedelta expressed in milliseconds.
         msToH: function (ms) {
             return parseInt((ms / 1000 / 60 / 60) % 24, 10);
         },
 
-        // Returns the number of days of the specified timedelta expressed in milliseconds.
+        // Extract the number of days from the given timedelta expressed in milliseconds.
         msToD: function (ms) {
             return parseInt((ms / 1000 / 60 / 60 / 24), 10);
         },
@@ -379,39 +382,39 @@
                     '<span class="label label-ss">', this.options.label_ss, '</span>',
                 '</span>'
             );
-            this.time_element.html(html.join(''));
+            this.timeElement.html(html.join(''));
             // Customize HTML according to options.
             if (!this.options.with_labels) {
-                this.time_element.find('.label').remove();
+                this.timeElement.find('.label').remove();
             }
             if (!this.options.with_separators) {
-                this.time_element.find('.separator').remove();
+                this.timeElement.find('.separator').remove();
             }
             if (!this.options.with_seconds) {
-                this.time_element.find('.item-ss').remove();
-                this.time_element.find('.separator').last().remove();
+                this.timeElement.find('.item-ss').remove();
+                this.timeElement.find('.separator').last().remove();
             }
             // Cache elements.
-            this.item_dd       = this.time_element.find('.item-dd');
-            this.separator_dd  = this.time_element.find('.separator-dd');
-            this.remaining_dd  = this.time_element.find('.dd');
-            this.remaining_hh1 = this.time_element.find('.hh-1');
-            this.remaining_hh2 = this.time_element.find('.hh-2');
-            this.remaining_mm1 = this.time_element.find('.mm-1');
-            this.remaining_mm2 = this.time_element.find('.mm-2');
-            this.remaining_ss1 = this.time_element.find('.ss-1');
-            this.remaining_ss2 = this.time_element.find('.ss-2');
+            this.item_dd       = this.timeElement.find('.item-dd');
+            this.separator_dd  = this.timeElement.find('.separator-dd');
+            this.remaining_dd  = this.timeElement.find('.dd');
+            this.remaining_hh1 = this.timeElement.find('.hh-1');
+            this.remaining_hh2 = this.timeElement.find('.hh-2');
+            this.remaining_mm1 = this.timeElement.find('.mm-1');
+            this.remaining_mm2 = this.timeElement.find('.mm-2');
+            this.remaining_ss1 = this.timeElement.find('.ss-1');
+            this.remaining_ss2 = this.timeElement.find('.ss-2');
             // Set the css class of the <time> element.
-            this.time_element.addClass(this.options.css_class);
+            this.timeElement.addClass(this.options.css_class);
         },
 
         doCountDown: function () {
             // Calculate the difference between the two dates in milliseconds.
-            // Note: in iOS JavaScript is paused during elastic scroll and not resumed until the scrolling stops.
+            // Note: in old iOS, JavaScript is paused during elastic scroll and not resumed until the scrolling stops.
             // Therefore we have to evaluate the remaining time with a new Date() object instead of assuming that
-            // setTimeout() will always be executed after the specified `set_timeout_delay` which would have allowed us
-            // to call the doCountDown() function with (ms - this.set_timeout_delay) as argument.
-            var ms = this.end_date.getTime() - new Date().getTime();
+            // setTimeout() will always be executed after the specified `setTimeoutDelay` which would have allowed us
+            // to call the doCountDown() function with (ms - this.setTimeoutDelay) as argument.
+            var ms = this.endDate.getTime() - new Date().getTime();
             // Extract seconds, minutes, hours and days from the timedelta expressed in milliseconds.
             var ss = this.msToS(ms);
             var mm = this.msToM(ms);
@@ -433,18 +436,18 @@
                 ss = 0;
             }
             if (dd === 0 && mm === 0 && hh === 0 && ss === 0) {
-                return this.time_element.trigger('time.elapsed');
+                return this.timeElement.trigger('time.elapsed');
             }
             // Reload it.
             var self = this;
-            window.setTimeout(function () { self.doCountDown(); }, self.set_timeout_delay);
+            window.setTimeout(function () { self.doCountDown(); }, self.setTimeoutDelay);
         },
 
         // @param remaining: an object literal containing a string representation of days, hours, minutes and
         // seconds remaining. e.g. { dd: "600", hh: "03", mm: "59", ss: "11" }
         displayRemainingTime: function (remaining) {
             // Format the datetime attribute of the <time> element to an ISO 8601 duration.
-            // http://www.whatwg.org/specs/web-apps/current-work/multipage/text-level-semantics.html#datetime-value
+            // https://html.spec.whatwg.org/multipage/semantics.html#datetime-value
             // i.e.: <time datetime="P2DT00H00M30S">2 00:00:00</time>
             var attr = [];
             attr.push('P');
@@ -455,7 +458,7 @@
             if (this.options.with_seconds) {
                 attr.push(remaining.ss, 'S');
             }
-            this.time_element.attr('datetime', attr.join(''));
+            this.timeElement.attr('datetime', attr.join(''));
             // Hide days if necessary.
             if (!this.options.always_show_days && remaining.dd === '0') {
                 this.item_dd.hide();
